dc78916064c8c5b75505f7f59edd3e43e38b2a56
aieuo <35859665+aieuo@users.noreply.github.com>
2025-04-01 18:15:03 +0900

    都道府県のチェックボックスがチェックされた時のイベントを受け取れるように

--- src/app/components/PrefectureList/PrefectureList.tsx
+++ src/app/components/PrefectureList/PrefectureList.tsx
@@ -4,11 +4,13 @@ import CheckBox from "../CheckBox/CheckBox";
 interface PrefectureListProps {
   prefectures: Prefecture[];
   selectedPrefectureCodes?: number[];
+  onChecked?: (code: number, checked: boolean) => void;
 }
 
 export default function PrefectureList({
   prefectures,
   selectedPrefectureCodes = [],
+  onChecked,
 }: PrefectureListProps) {
   return (
     <div className="flex flex-wrap gap-4">
@@ -17,6 +19,9 @@ export default function PrefectureList({
           label={prefecture.prefName}
           key={prefecture.prefCode}
           checked={selectedPrefectureCodes.includes(prefecture.prefCode)}
+          onChecked={(checked) =>
+            onChecked && onChecked(prefecture.prefCode, checked)
+          }
         />
       ))}
     </div>
