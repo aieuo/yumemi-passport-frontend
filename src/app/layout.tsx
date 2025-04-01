e747243912f24a2f5f9eee9ef19e14626a59062b
aieuo <35859665+aieuo@users.noreply.github.com>
2025-04-01 18:37:24 +0900

    Header追加

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import Header from "@/components/Header/Header";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import React from "react";
@@ -26,6 +27,7 @@ export default function RootLayout({
   return (
     <html lang="ja">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        <Header />
         {children}
       </body>
     </html>
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,9 @@
+export default function Header() {
+  return (
+    <header className="bg-gray-900 p-4 text-center">
+      <h1 className="text-2xl font-extrabold text-white">
+        都道府県別の総人口推移グラフ
+      </h1>
+    </header>
+  );
+}
